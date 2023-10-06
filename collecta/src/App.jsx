82681357de992fe7d7bcfa26e94e0055628ec1de
the-cris-code <cristhian.silva@sptech.school>
2023-10-06 17:03:05 -0300

    arrumando as cagadas da emi

--- collecta/src/App.jsx
+++ collecta/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
+
 import "../styles/global.css";
 import "./components/navbar/navbar.css";
-import Logo from "./assets/logo/logo-collecta.svg";
-import Pacoca from "./assets/logo/pacoca.jpg"
+
 import Navbar from "./components/navbar/Navbar"
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <body>
         <Navbar/>
       </body>
