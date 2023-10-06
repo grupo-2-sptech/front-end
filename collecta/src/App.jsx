b9661f1c10205eb66df4dbde5d7818c357172363
emilliedev <emillie.souza@sptech.school>
2023-10-06 16:37:58 -0300

    feat/componentizando navbar na App

--- collecta/src/App.jsx
+++ collecta/src/App.jsx
@@ -3,6 +3,7 @@ import "../styles/global.css";
 import "./components/navbar/navbar.css";
 import Logo from "./assets/logo/logo-collecta.svg";
 import Pacoca from "./assets/logo/pacoca.jpg"
+import Navbar from "./components/navbar/Navbar"
 // import './App.css'
 
 function App() {
@@ -12,37 +13,7 @@ function App() {
     <>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <body>
-        <header>
-          <div className="header-bg">
-            <div className="container">
-              <div className="header-container d-flex jc-between">
-                <img
-                  src={Logo}
-                  alt="Logo da Collecta, a letra C e O com tons de azul"
-                />
-                <section className="header-item body-medium-bold d-flex ai-center">
-                  <span className="color-white">Criar campanha</span>
-                </section>
-                <section className="header-item body-medium-bold d-flex ai-center">
-                  <span className="color-white">Explorar</span>
-                </section>
-                <section className="conjunto-barra-busca-projeto d-flex">
-                  <section>
-                    <input type="text" className="barra-busca-projetos" placeholder="Busque projetos..."/>
-                  </section>
-                  <button className="botao-pesquisar">
-                  <span class="material-symbols-outlined color-white">search</span>
-                  </button>
-                </section>
-                <section>
-                  <div className="icone-perfil">
-                    <img src={Pacoca} alt="" className="img-perfil"/>
-                  </div>
-                </section>
-              </div>
-            </div>
-          </div>
-        </header>
+        <Navbar/>
       </body>
     </>
   );
--- collecta/src/components/navbar/Navbar.jsx
+++ collecta/src/components/navbar/Navbar.jsx
@@ -2,7 +2,37 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      
+      <header>
+          <div className="header-bg">
+            <div className="container">
+              <div className="header-container d-flex jc-between">
+                <img
+                  src={Logo}
+                  alt="Logo da Collecta, a letra C e O com tons de azul"
+                />
+                <section className="header-item body-medium-bold d-flex ai-center">
+                  <span className="color-white">Criar campanha</span>
+                </section>
+                <section className="header-item body-medium-bold d-flex ai-center">
+                  <span className="color-white">Explorar</span>
+                </section>
+                <section className="conjunto-barra-busca-projeto d-flex">
+                  <section>
+                    <input type="text" className="barra-busca-projetos" placeholder="Busque projetos..."/>
+                  </section>
+                  <button className="botao-pesquisar">
+                  <span class="material-symbols-outlined color-white">search</span>
+                  </button>
+                </section>
+                <section>
+                  <div className="icone-perfil">
+                    <img src={Pacoca} alt="" className="img-perfil"/>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </header>
     </>
   );
 }
