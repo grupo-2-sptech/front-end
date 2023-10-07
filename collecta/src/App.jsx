fb4256467b8045ca6b3eff3c3ebea1ec892005fd
emilliedev <emillie.souza@sptech.school>
2023-10-07 01:14:05 -0300

    feat/cards com props

--- collecta/src/App.jsx
+++ collecta/src/App.jsx
@@ -1,16 +1,90 @@
 import "../styles/global.css";
+import acao from "./assets/img/acao.png";
+import adapta from "./assets/img/adapta.png";
+import atlantica from "./assets/img/atlantica.png";
+import aujuda from "./assets/img/aujuda.png";
+import banho from "./assets/img/banho.png";
+import marmitada from "./assets/img/marmitada.png";
 
 import Navbar from "./components/navbar/Navbar";
 import Card from "./components/card/Card";
 
-// import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      
-        <Navbar />
-        {/* <Card /> */}
+      <Navbar />
+      <div className="container">
+          <div className="card-box jc-between">
+            <Card
+              titulo="Oie"
+              responsavel="Vish"
+              texto="Tudo"
+              porcentagem="89"
+              valor="38.365"
+              local="Santa Cruz da Baixa Verde - PE"
+              tag="Socioambiental"
+              img={acao}
+            />
+
+            <Card
+              titulo="Oie"
+              responsavel="Vish"
+              texto="Tudo"
+              porcentagem="89"
+              valor="38.365"
+              local="Santa Cruz da Baixa Verde - PE"
+              tag="Socioambiental"
+              img={acao}
+            />
+
+            <Card
+              titulo="Oie"
+              responsavel="Vish"
+              texto="Tudo"
+              porcentagem="89"
+              valor="38.365"
+              local="Santa Cruz da Baixa Verde - PE"
+              tag="Socioambiental"
+              img={acao}
+            />
+          </div>
+          <div className="card-box">
+            <Card
+              titulo="Oie"
+              responsavel="Vish"
+              texto="Tudo"
+              porcentagem="89"
+              valor="38.365"
+              local="Santa Cruz da Baixa Verde - PE"
+              tag="Socioambiental"
+              img={acao}
+            />
+
+            <Card
+              titulo="Oie"
+              responsavel="Vish"
+              texto="Tudo"
+              porcentagem="89"
+              valor="38.365"
+              local="Santa Cruz da Baixa Verde - PE"
+              tag="Socioambiental"
+              img={acao}
+            />
+
+            <Card
+              titulo="Oie"
+              responsavel="Vish"
+              texto="Tudo"
+              porcentagem="89"
+              valor="38.365"
+              local="Santa Cruz da Baixa Verde - PE"
+              tag="Socioambiental"
+              img={acao}
+            />
+          </div>
+        </div>
       
     </>
   );
--- collecta/src/components/card/Card.jsx
+++ collecta/src/components/card/Card.jsx
@@ -1,14 +1,13 @@
-import Pacoca from "../../assets/logo/pacoca.jpg";
 import iRecorrente from "../../assets/icon/i-recorrente.svg";
 import iLocal from "../../assets/icon/i-local.svg";
 import iTag from "../../assets/icon/i-tag.svg";
 import "../../components/card/Card.css";
 
-function Card() {
+function Card(props) {
   return (
     <div className="card border-ice">
       <section className="card-thumbnail mb-16">
-        <img src={Pacoca} className="card-thumb-img" />
+        <img src={props.img} className="card-thumb-img" />
 
         <span className="card-pin-destaque body-xsmall color-haiti bg-ice">
           Projeto destaque
@@ -16,30 +15,25 @@ function Card() {
       </section>
       <section className="p-16">
         <section className="card-texto d-flex fd-column">
-          <span className="card-titulo body-large mb-22">Título</span>
+          <span className="card-titulo body-large mb-22">{props.titulo}</span>
           <span className="card-responsavel body-small mb-22">
-            por responsavel
+            por {props.responsavel}
           </span>
           <span className="body-small mb-32">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
-            officiis voluptatibus ur soluta recusandae veniam! Ex voluptatibus
-            doloribus nobis aspernatur accusamus, eaque debitis! Nulla eaque
-            asperiores, eveniet omnis{" "}
+            {props.texto}
           </span>
         </section>
         <progress
           className="mb-32 card-progress-bar"
           id="file"
-          value="32"
+          value="89"
           max="100"
         >
-          {" "}
-          32%{" "}
         </progress>
         <section className="card-numeros mb-22 d-flex jc-between ai-center">
-          <span className="body-large">0%</span>
+          <span className="body-large">{props.porcentagem}%</span>
           <section className="d-flex fd-column">
-            <span className="body-large">R$ 0.000</span>
+            <span className="body-large">R$ {props.valor}</span>
             <span className="body-xsmall">Levantados</span>
           </section>
           <img src={iRecorrente} />
@@ -48,11 +42,11 @@ function Card() {
       <section className="card-rodape p-16-lateral p-8-vertical bg-ice">
         <section className="card-local mb-8 d-flex">
           <img src={iLocal} />
-          <span className="body-xsmall ml-8">São Paulo - SP</span>
+          <span className="body-xsmall ml-8">{props.local}</span>
         </section>
         <section className="card-tag d-flex">
           <img src={iTag} />
-          <span className="body-xsmall ml-8">Animais</span>
+          <span className="body-xsmall ml-8">{props.tag}</span>
         </section>
       </section>
     </div>
