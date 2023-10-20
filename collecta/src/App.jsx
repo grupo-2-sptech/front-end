d2e39aec0a0373c06ec0f0b45ae913b232999f8a
emilliedev <emillie.souza@sptech.school>
2023-10-20 17:41:18 -0300

    feat/hero completo

--- collecta/src/App.jsx
+++ collecta/src/App.jsx
@@ -8,11 +8,12 @@ import CardPontual from "./components/card-pontual/CardPontual";
 import "./App.css";
 import "./components/card/card.css";
 import costura01 from "./assets/background/costura01.svg";
-import iGrafico from './assets/icon/i-grafico.svg';
-import iAjuda from './assets/icon/i-ajuda.svg';
-import iMaos from './assets/icon/i-maos.svg';
-import iOlho from './assets/icon/i-olho.svg';
-import logoNome from './assets/logo/logo-nome.svg'
+import iGrafico from "./assets/icon/i-grafico.svg";
+import iAjuda from "./assets/icon/i-ajuda.svg";
+import iMaos from "./assets/icon/i-maos.svg";
+import iOlho from "./assets/icon/i-olho.svg";
+import logoNome from "./assets/logo/logo-nome.svg";
+import maos from "./assets/img/maos.png";
 
 function App() {
   const p = 54;
@@ -22,6 +23,25 @@ function App() {
   return (
     <>
       <Navbar />
+      <div className="hero-container w-100 mb-100">
+        <img src={maos} className="hero-background" />
+        <div className="container">
+          <div className="pos-absolute tet">
+        <span className="color-ice texto-hero head-large">
+          <span className="texto1-hero">
+          Promover o <span className="color-white">bem</span> e
+          <span className="color-white"> Conectar</span> pessoas
+          </span>
+          <br />
+          <span className="color-ice">
+            FAZEMOS ISSO <span className="color-white">JUNTOS</span>
+          </span>
+          <br />
+          <button className="p-16-lateral p-8-vertical body-medium br-5 border-none bg-chambray color-white cursor-pointer">SAIBA MAIS</button>
+        </span>
+        </div>
+        </div>
+      </div>
       <div className="container">
         <div className="card-heading d-flex jc-between ai-center mb-32">
           <span className="head-medium color-haiti">Campanhas assinatura</span>
@@ -128,47 +148,61 @@ function App() {
               Venha fazer parte dessa história!
             </span>
             <div>
-              <button className="btn-criar-campanha border-none bg-chambray color-white br-5 body-large">CRIAR CAMPANHA</button>
+              <button className="btn-criar-campanha border-none bg-chambray color-white br-5 body-large">
+                CRIAR CAMPANHA
+              </button>
             </div>
           </span>
         </div>
       </div>
       <img src={costura01} className="w-100 mb-128" />
       <div className="container">
-      <section className="text-align-center">
-        <span className="color-haiti head-large" >
-          Sobre a <span className="color-chambray">Collectiva</span>
-        </span>
-        <div className="container-icones-sobre d-flex jc-between mt-100">
-        <section className="sect-icone-sobre">
-          <section className="">
-          <img src={iMaos} className="mb-16"/>
-          <span className="body-xlarge texto-icone">Vivenciamos de perto a mudança ativa na sociedade, fauna e flora</span>
-          </section>
+        <section className="text-align-center">
+          <span className="color-haiti head-large">
+            Sobre a <span className="color-chambray">Collectiva</span>
+          </span>
+          <div className="container-icones-sobre d-flex jc-between mt-100">
+            <section className="sect-icone-sobre">
+              <section className="">
+                <img src={iMaos} className="mb-16" />
+                <span className="body-xlarge texto-icone">
+                  Vivenciamos de perto a mudança ativa na sociedade, fauna e
+                  flora
+                </span>
+              </section>
+            </section>
+            <section className="sect-icone-sobre d-flex">
+              <section className="">
+                <img src={iAjuda} className="mb-16" />
+                <span className="body-xlarge texto-icone">
+                  Vivenciamos de perto a mudança ativa na sociedade, fauna e
+                  flora
+                </span>
+              </section>
+            </section>
+            <section className="sect-icone-sobre d-flex">
+              <section className="">
+                <img src={iOlho} className="mb-16" />
+                <span className="body-xlarge texto-icone">
+                  Vivenciamos de perto a mudança ativa na sociedade, fauna e
+                  flora
+                </span>
+              </section>
+            </section>
+            <section className="sect-icone-sobre d-flex">
+              <section className="">
+                <img src={iGrafico} className="mb-16" />
+                <span className="body-xlarge texto-icone">
+                  Vivenciamos de perto a mudança ativa na sociedade, fauna e
+                  flora
+                </span>
+              </section>
+            </section>
+          </div>
         </section>
-        <section className="sect-icone-sobre d-flex">
-          <section className="">
-          <img src={iAjuda} className="mb-16"/>
-          <span className="body-xlarge texto-icone">Vivenciamos de perto a mudança ativa na sociedade, fauna e flora</span>
-          </section>
-        </section>
-        <section className="sect-icone-sobre d-flex">
-          <section className="">
-          <img src={iOlho} className="mb-16"/>
-          <span className="body-xlarge texto-icone">Vivenciamos de perto a mudança ativa na sociedade, fauna e flora</span>
-          </section>
-        </section>
-        <section className="sect-icone-sobre d-flex">
-          <section className="">
-          <img src={iGrafico} className="mb-16"/>
-          <span className="body-xlarge texto-icone">Vivenciamos de perto a mudança ativa na sociedade, fauna e flora</span>
-          </section>
-        </section>
-        </div>
-      </section>
       </div>
       <footer className="footer bg-chambray mt-128 d-flex jc-center ai-center">
-        <img src={logoNome}/>
+        <img src={logoNome} />
       </footer>
     </>
   );
