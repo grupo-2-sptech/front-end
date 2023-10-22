29297e1c36c62302e27dd8fbc7847c959ec99dc0
emilliedev <emillie.souza@sptech.school>
2023-10-22 19:46:12 -0300

    fix/conflito import

--- collecta/src/App.jsx
+++ collecta/src/App.jsx
@@ -1,18 +1,10 @@
 import "../styles/global.css";
 
-import imagens from "./assets/img/ImagensExternas";
-
-
-import Navbar from "./components/navbar/Navbar";
-import Card from "./components/card/Card";
-import CardPontual from "./components/card-pontual/CardPontual";
-
 import "./App.css";
 import "./components/card/card.css";
 import Rotas from "./Rotas"
 
 import { BrowserRouter as Router } from 'react-router-dom';
-
 
 function App() {
   return (
