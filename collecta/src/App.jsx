230ee447f245dd14e3c00fc3ee04db642b61f8bb
emilliedev <emillie.souza@sptech.school>
2023-11-18 02:22:51 -0300

    fix/tela criar campanha refatorada de acordo com prototipo

--- collecta/src/App.jsx
+++ collecta/src/App.jsx
@@ -3,14 +3,11 @@ import "../styles/global.css";
 import "./App.css";
 import "./components/card/card.css";
 import Rotas from "./Rotas";
-// import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <>
-
-    <Rotas/>
-  
+      <Rotas/>
     </>
   );
 }
