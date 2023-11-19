7bf3e4dd989522764960db7cbb23e5340a951a14
AlexVictorAndrade <alex.andrade@sptech.school>
2023-11-19 14:24:51 -0300

    feature-cadastrar-ong-done

--- collecta/src/Rotas.jsx
+++ collecta/src/Rotas.jsx
@@ -1,11 +1,12 @@
 
-import Index from "./pages/index/index";
+import Index from "./pages/index/Index";
 import Campanha from "./pages/campanha/Campanha";
 import Cadastro from "./components/form-cadastro/FormCadastro";
 import Login from "./pages/login/login";
 import Voluntariado from "./pages/voluntariado/Voluntariado";
 import FeedOng from "./pages/feed/FeedOng";
 import Pagamento from "./pages/pagamento/Pagamento.jsx";
+import CadastroOng from "./pages/cadastro-ong/CadastroOng"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function Rotas() {
@@ -20,7 +21,7 @@ function Rotas() {
           <Route path="/voluntariado" element={<Voluntariado />} />
           <Route path="/feed-ong" element={<FeedOng />} />
           <Route path="/pagamento" element={<Pagamento />} />
-
+          <Route path="/cadastro-ong" element={<CadastroOng/>}/>
         </Routes>
       </BrowserRouter>
     </>
