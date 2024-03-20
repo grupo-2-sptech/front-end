213e7b5d5121717a59369153ebf83c0b6d26d533
the-cris-code <cristhian.silva@sptech.school>
2024-03-19 21:51:36 -0300

    config vite

--- collecta/vite.config.js
+++ collecta/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions:{
+      input: {
+        main: 'index.html'
+      },
+    },
+  },
+  server: {
+    port: 5173, 
+    host: '0.0.0.0'
+  },
 })
